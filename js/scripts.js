9561f481885b06eabe83c81c776ab94ed200d86f
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2022-10-30 13:27:03 +0200

    Done the homework

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    navText: [ '', ' ' ],
+    autoplay: true,
+    autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:1
+        },
+
+        768:{
+            items:2
+        },
+
+        1000:{
+            items:3
+        }
+    }
+});
+
+
